b58a8cf3736ac92e6778648f39bb8aa53c93036f
matan <matan.sh@bluewhite.co>
2026-05-18 13:58:40 +0300

    increase config section

--- interfaces/interfaces_g474/MemoryMap/memory_map.hpp
+++ interfaces/interfaces_g474/MemoryMap/memory_map.hpp
@@ -9,11 +9,11 @@
 constexpr static uint32_t FLASH_BW_BOOTLOADER_ADDRESS = 0x08000000;
 constexpr static uint32_t FLASH_META_DATA_ADDRESS = 0x08010000;
 constexpr static uint32_t FLASH_APPLICATION_ADDRESS = 0x08010800;
-constexpr static uint32_t FLASH_CONFIG_ADDRESS = 0x0807F000;
+constexpr static uint32_t FLASH_CONFIG_ADDRESS = 0x08070000;
 
 constexpr static uint32_t FLASH_BW_BOOTLOADER_BYTES_SIZE = 0x10000; // 64K in hex
 constexpr static uint32_t FLASH_META_DATA_BYTES_SIZE = 0x800; // 2K in hex
-constexpr static uint32_t FLASH_APPLICATION_BYTES_SIZE = 0x6E800; // 442K in hex
-constexpr static uint32_t FLASH_CONFIG_BYTES_SIZE = 0x1000; // 4K in hex
+constexpr static uint32_t FLASH_APPLICATION_BYTES_SIZE = 0x5F800; // 374.5K in hex
+constexpr static uint32_t FLASH_CONFIG_BYTES_SIZE = 0x10000; // 64K in hex
 
 #endif // FW_LLC_INTERFACES_MEMORY_MAP_HPP_
